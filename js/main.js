68996692f9081f52ca3ba5ed8d6a1e193dadc4d5
ELCRND <jwhh2a@gmail.com>
2024-08-02 11:23:45 +0300

    modified:   css/styles.css 	modified:   css/styles.css.map 	new file:   favicon.png 	modified:   index.html 	modified:   js/main.js 	modified:   scss/blocks/buy.scss 	modified:   scss/blocks/footer.scss 	modified:   scss/blocks/header.scss 	modified:   scss/blocks/home.scss

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const code = document.querySelector(".token__code");
 const tokenomicCode = document.querySelector(".tokenomic__token-code");
+const footerCode = document.querySelector(".footer__token-code");
 const tooltip = document.querySelector(".tooltip");
 
 // копирование кода в буфер обмена
@@ -15,3 +16,8 @@ document
     tooltip.classList.add("visible");
     setTimeout(() => tooltip.classList.remove("visible"), 600);
   });
+document.querySelector(".footer__token-copy").addEventListener("click", () => {
+  navigator.clipboard.writeText(footerCode.innerText);
+  tooltip.classList.add("visible");
+  setTimeout(() => tooltip.classList.remove("visible"), 600);
+});
